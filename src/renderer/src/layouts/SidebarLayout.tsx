b4be8c2b3c614f2eb84aa3cf6a44790e3bd2a5e0
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-19 23:35:22 +0100

    Updated sidebar layout

--- src/renderer/src/layouts/SidebarLayout.tsx
+++ src/renderer/src/layouts/SidebarLayout.tsx
@@ -7,8 +7,10 @@ import {
   ArrowBigUpIcon,
   AwardIcon,
   Building2Icon,
+  BuildingIcon,
   IdCardLanyardIcon,
-  UserPenIcon
+  UserPenIcon,
+  Users2Icon
 } from 'lucide-react';
 import { JSX } from 'react/jsx-runtime';
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
@@ -34,112 +36,116 @@ const SidebarLayout = (): JSX.Element => {
             {
               label: 'Departments',
               key: 'departments',
+              icon: <BuildingIcon size={15} />,
+              onClick: () => navigate('/departments')
+              // children: [
+              //   {
+              //     label: 'List',
+              //     key: 'departments-list',
+              //     onClick: () => navigate('/departments')
+              //   },
+              //   {
+              //     label: 'Register',
+              //     key: 'departments-register',
+              //     onClick: () => navigate('/departments/register')
+              //   },
+              //   {
+              //     label: 'Import',
+              //     key: 'departments-import',
+              //     onClick: () => navigate('/departments/import')
+              //   }
+              // ]
+            },
+            {
+              label: 'Sub Departments',
+              key: 'sub-departments',
               icon: <Building2Icon size={15} />,
-              children: [
-                {
-                  label: 'List',
-                  key: 'departments-list',
-                  onClick: () => navigate('/departments')
-                },
-                {
-                  label: 'Register',
-                  key: 'departments-register',
-                  onClick: () => navigate('/departments/register')
-                },
-                {
-                  label: 'Import',
-                  key: 'departments-import',
-                  onClick: () => navigate('/departments/import')
-                },
-                {
-                  type: 'submenu',
-                  label: 'Sub Departments',
-                  key: 'sub-departments',
-                  children: [
-                    { label: 'List', key: 'sub-departments-list' },
-                    {
-                      type: 'submenu',
-                      key: 'units',
-                      label: 'Units',
-                      children: [{ label: 'List', key: 'units-list' }]
-                    }
-                  ]
-                }
-              ]
+              onClick: () => navigate('/sub-departments')
+            },
+            {
+              key: 'units',
+              label: 'Units',
+              icon: <Users2Icon size={15} />,
+              onClick: () => navigate('/units')
             },
             {
               label: 'Employees',
               key: 'employees',
               icon: <IdCardLanyardIcon size={15} />,
-              children: [
-                {
-                  label: 'List',
-                  key: 'employees-list',
-                  onClick: () => navigate('/employees')
-                },
-                {
-                  label: 'Register',
-                  key: 'employees-register',
-                  onClick: () => navigate('/employees/register')
-                },
-                {
-                  label: 'Import',
-                  key: 'employees-import',
-                  onClick: () => navigate('/employees/import')
-                }
-              ]
+              onClick: () => navigate('/employees')
+              // children: [
+              //   {
+              //     label: 'List',
+              //     key: 'employees-list',
+              //     onClick: () => navigate('/employees')
+              //   },
+              //   {
+              //     label: 'Register',
+              //     key: 'employees-register',
+              //     onClick: () => navigate('/employees/register')
+              //   },
+              //   {
+              //     label: 'Import',
+              //     key: 'employees-import',
+              //     onClick: () => navigate('/employees/import')
+              //   }
+              // ]
             },
             {
               label: 'Positions',
               key: 'positions',
               icon: <UserPenIcon size={15} />,
-              children: [
-                {
-                  label: 'List',
-                  key: 'positions-list',
-                  onClick: () => navigate('/positions')
-                },
-                {
-                  label: 'Register',
-                  key: 'positions-register',
-                  onClick: () => navigate('/positions/register')
-                }
-              ]
+              onClick: () => navigate('/positions')
+              // children: [
+              //   {
+              //     label: 'List',
+              //     key: 'positions-list',
+              //     onClick: () => navigate('/positions')
+              //   },
+              //   {
+              //     label: 'Register',
+              //     key: 'positions-register',
+              //     onClick: () => navigate('/positions/register')
+              //   }
+              // ]
             },
             {
               label: 'Awards/Disciplinary Actions',
               key: 'awards',
               icon: <AwardIcon size={15} />,
-              children: [
-                { label: 'List', key: 'awards-list', onClick: () => navigate('/awards') },
-                {
-                  label: 'Register',
-                  key: 'awards-register',
-                  onClick: () => navigate('/awards/register')
-                }
-              ]
+              onClick: () => navigate('/awards')
+              // children: [
+              //   { label: 'List', key: 'awards-list', onClick: () => navigate('/awards') },
+              //   {
+              //     label: 'Register',
+              //     key: 'awards-register',
+              //     onClick: () => navigate('/awards/register')
+              //   }
+              // ]
             },
             {
               label: 'Performance Evaluations',
               key: 'performance-evaluations',
               icon: <RadarChartOutlined />,
-              children: [
-                {
-                  label: 'List',
-                  key: 'performance-evaluations-list',
-                  onClick: () => navigate('/performance-evaluations')
-                },
-                {
-                  label: 'Register',
-                  key: 'performance-evaluations-register',
-                  onClick: () => navigate('/performance-evaluations/register')
-                }
-              ]
+              onClick: () => navigate('/performance-evaluations')
+              // children: [
+              //   {
+              //     label: 'List',
+              //     key: 'performance-evaluations-list',
+              //     onClick: () => navigate('/performance-evaluations')
+              //   },
+              //   {
+              //     label: 'Register',
+              //     key: 'performance-evaluations-register',
+              //     onClick: () => navigate('/performance-evaluations/register')
+              //   }
+              // ]
             },
             {
               label: 'Promotions',
               key: 'promotions',
-              icon: <ArrowBigUpIcon size={15} />
+              icon: <ArrowBigUpIcon size={15} />,
+              onClick: () => navigate('/promotions')
             }
           ]}
         />
