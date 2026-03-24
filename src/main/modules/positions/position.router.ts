16dc539a9617ed6e69111877b15e80346b93e467
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-24 20:37:51 +0100

    Implement register position API

--- src/main/modules/positions/position.router.ts
+++ src/main/modules/positions/position.router.ts
@@ -1,7 +1,14 @@
+import { container } from 'tsyringe';
+
+import { registerPositionRequest } from '../../../shared/dto/positions/register-positions.dto';
 import t from '../../trpc';
+import { PositionService } from './position.service';
 
 const positionRouter = t.router({
-  registerPosition: t.procedure.query(() => {})
+  registerPosition: t.procedure.input(registerPositionRequest).query(async (c) => {
+    const positionService = container.resolve(PositionService);
+    await positionService.registerPosition(c.input);
+  })
 });
 
 export default positionRouter;
--- src/main/modules/positions/position.service.ts
+++ src/main/modules/positions/position.service.ts
@@ -1,6 +1,8 @@
 import { container, injectable } from 'tsyringe';
 
+import { RegisterPositionRequest } from '../../../shared/dto/positions/register-positions.dto';
 import { DatabaseType } from '../../db';
+import { jobGrades, NewJobGrade, NewPosition, positions } from '../../db/schema';
 import { SessionInfo } from '../auth/session-info';
 
 @injectable()
@@ -11,5 +13,26 @@ export class PositionService {
   constructor() {
     this.db = container.resolve<DatabaseType>('Database');
     this.sessionInfo = container.resolve(SessionInfo);
+  }
+
+  async registerPosition(req: RegisterPositionRequest): Promise<void> {
+    const newPosition: NewPosition = {
+      name: req.name,
+      code: req.code,
+      description: req.description
+    };
+    const { lastInsertRowid: positionId } = await this.db.insert(positions).values(newPosition);
+
+    const newJobGrades: NewJobGrade[] = req.jobGrades.map((grade) => ({
+      level: grade.level,
+      minSalary: grade.minSalary,
+      maxSalary: grade.maxSalary,
+      timeInRole: grade.timeInRole,
+      description: grade.description,
+      headcount: grade.headcount,
+      positionId: positionId as number
+    }));
+
+    await this.db.insert(jobGrades).values(newJobGrades);
   }
 }
