6e85d1d25c72319c44a15b3cb04081aa6b4fed91
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-24 18:47:16 +0100

    Implement register/find unit service

--- src/main/modules/units/unit.router.ts
+++ src/main/modules/units/unit.router.ts
@@ -1,8 +1,19 @@
+import { container } from 'tsyringe';
+
+import { findUnitRequest } from '../../../shared/dto/units/find-unit.dto';
+import { registerUnitRequest } from '../../../shared/dto/units/register-unit.dto';
 import t from '../../trpc';
+import { UnitService } from './unit.service';
 
 const unitRouter = t.router({
-  registerUnit: t.procedure.mutation(() => {}),
-  findUnit: t.procedure.query(() => {})
+  registerUnit: t.procedure.input(registerUnitRequest).mutation(async (c) => {
+    const unitService = container.resolve(UnitService);
+    await unitService.registerUnit(c.input);
+  }),
+  findUnit: t.procedure.input(findUnitRequest).query(async (c) => {
+    const unitService = container.resolve(UnitService);
+    return await unitService.findUnit(c.input);
+  })
 });
 
 export default unitRouter;
--- src/main/modules/units/unit.service.ts
+++ src/main/modules/units/unit.service.ts
@@ -1,6 +1,10 @@
+import { and, eq, like } from 'drizzle-orm';
 import { container, injectable } from 'tsyringe';
 
+import { FindUnitRequest, FindUnitResponse } from '../../../shared/dto/units/find-unit.dto';
+import { RegisterUnitRequest } from '../../../shared/dto/units/register-unit.dto';
 import { DatabaseType } from '../../db';
+import { NewOrganizationalUnit, organizationalUnits } from '../../db/schema';
 import { SessionInfo } from '../auth/session-info';
 
 @injectable()
@@ -11,5 +15,51 @@ export class UnitService {
   constructor() {
     this.db = container.resolve<DatabaseType>('Database');
     this.sessionInfo = container.resolve(SessionInfo);
+  }
+
+  async registerUnit(req: RegisterUnitRequest): Promise<void> {
+    const newUnit: NewOrganizationalUnit = {
+      code: req.code,
+      name: req.name,
+      status: req.status,
+      type: 'UNIT',
+      description: req.description,
+      parentId: req.subDepartmentId
+    };
+
+    const result = await this.db.insert(organizationalUnits).values(newUnit);
+    if (result.changes === 0) throw new Error('Something went wrong');
+  }
+
+  async findUnit(req: FindUnitRequest): Promise<FindUnitResponse> {
+    const where = and(
+      eq(organizationalUnits.type, 'UNIT'),
+      ...(req.name ? [like(organizationalUnits.name, `%${req.name}%`)] : []),
+      ...(req.status ? [eq(organizationalUnits.status, req.status)] : [])
+    );
+
+    const units = await this.db.query.organizationalUnits.findMany({
+      where,
+      offset: (req.page - 1) * 10,
+      limit: 10,
+      with: { parent: true }
+    });
+
+    const total = await this.db.$count(organizationalUnits, where);
+
+    return {
+      total,
+      items: units.map((unit) => ({
+        id: unit.id,
+        code: unit.code,
+        name: unit.name,
+        status: unit.status,
+        subDepartment: {
+          id: unit.parent?.id || 0,
+          code: unit.parent?.code || '',
+          name: unit.parent?.name || ''
+        }
+      }))
+    };
   }
 }
