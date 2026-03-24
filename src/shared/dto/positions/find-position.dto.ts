2aec6c88bf3ff95eb8b224ab39a409f0f8c85f42
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-24 21:23:31 +0100

    Implement find position DTO

---
+++ src/shared/dto/positions/find-position.dto.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const findPositionRequest = z.object({
+  page: z.coerce.number(),
+  name: z.string().nullable(),
+  code: z.string().nullable()
+});
+
+export const findPositionResponse = z.object({
+  total: z.number(),
+  items: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      code: z.string()
+    })
+  )
+});
+
+export type FindPositionRequest = z.infer<typeof findPositionRequest>;
+export type FindPositionResponse = z.infer<typeof findPositionResponse>;
