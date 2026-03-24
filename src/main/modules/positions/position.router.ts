63f43f79ac36a9c9fafa9b8c5dfb82d73702a5dc
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-24 21:29:30 +0100

    Implement find position API

--- src/main/modules/positions/position.router.ts
+++ src/main/modules/positions/position.router.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+import { findPositionRequest } from '../../../shared/dto/positions/find-position.dto';
 import { registerPositionRequest } from '../../../shared/dto/positions/register-positions.dto';
 import t from '../../trpc';
 import { PositionService } from './position.service';
@@ -8,6 +9,10 @@ const positionRouter = t.router({
   registerPosition: t.procedure.input(registerPositionRequest).mutation(async (c) => {
     const positionService = container.resolve(PositionService);
     await positionService.registerPosition(c.input);
+  }),
+  findPosition: t.procedure.input(findPositionRequest).query(async (c) => {
+    const positionService = container.resolve(PositionService);
+    return positionService.findPosition(c.input);
   }),
   getJobGradeLevelOptions: t.procedure.query(() => {
     const positionService = container.resolve(PositionService);
--- src/main/modules/positions/position.service.ts
+++ src/main/modules/positions/position.service.ts
@@ -1,6 +1,11 @@
+import { and, like } from 'drizzle-orm';
 import { container, injectable } from 'tsyringe';
 
 import { GetOptionsResponse } from '../../../shared/dto/get-options.dto';
+import {
+  FindPositionRequest,
+  FindPositionResponse
+} from '../../../shared/dto/positions/find-position.dto';
 import { RegisterPositionRequest } from '../../../shared/dto/positions/register-positions.dto';
 import { DatabaseType } from '../../db';
 import { jobGradeLevel, jobGrades, NewJobGrade, NewPosition, positions } from '../../db/schema';
@@ -39,5 +44,29 @@ export class PositionService {
 
   async getJobGradeLevelOptions(): Promise<GetOptionsResponse> {
     return jobGradeLevel.map((value) => ({ label: value, value }));
+  }
+
+  async findPosition(req: FindPositionRequest): Promise<FindPositionResponse> {
+    const where = and(
+      ...(req.name ? [like(positions.name, `%${req.name}%`)] : []),
+      ...(req.code ? [like(positions.code, `%%${req.code}`)] : [])
+    );
+
+    const positionList = await this.db.query.positions.findMany({
+      where,
+      offset: (req.page - 1) * 10,
+      limit: 10
+    });
+
+    const total = await this.db.$count(positions, where);
+
+    return {
+      total,
+      items: positionList.map((pos) => ({
+        id: pos.id,
+        code: pos.code,
+        name: pos.name
+      }))
+    };
   }
 }
