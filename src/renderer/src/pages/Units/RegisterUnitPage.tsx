df4c98f201de66203cfa70c06b9652043f41ca84
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-24 18:58:17 +0100

    Implement register unit page

--- src/renderer/src/pages/Units/RegisterUnitPage.tsx
+++ src/renderer/src/pages/Units/RegisterUnitPage.tsx
@@ -1,9 +1,120 @@
-import { Breadcrumb, Flex } from 'antd';
+import { CheckOutlined, LeftOutlined } from '@ant-design/icons';
+import { trpc } from '@renderer/trpc';
+import { App, Breadcrumb, Button, Descriptions, Flex, Form, Input, Select } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
+import { useNavigate } from 'react-router-dom';
+import { RegisterUnitRequest } from 'src/shared/dto/units/register-unit.dto';
 
 const RegisterUnitPage = () => {
+  const { message } = App.useApp();
+  const navigate = useNavigate();
+  const [form] = Form.useForm<RegisterUnitRequest>();
+  const { mutateAsync: register, isPending: registerPending } = trpc.units.registerUnit.useMutation(
+    {
+      onSuccess: () => navigate('/units'),
+      onError: (error) => {
+        console.log(error);
+        message.error('Failed to register');
+      }
+    }
+  );
+  const { data: subDepartmentOptions } = trpc.subDepartments.getSubDepartmentOptions.useQuery();
+
+  const submit = async () => {
+    const values = await form.validateFields();
+    await register(values);
+  };
+
   return (
     <Flex style={{ width: '100%', height: '100%', padding: '2rem' }} vertical gap="large">
       <Breadcrumb items={[{ title: 'Units' }, { title: 'Register' }]} />
+
+      <Form variant="filled" form={form}>
+        <Descriptions
+          bordered
+          column={2}
+          items={[
+            {
+              label: 'Name',
+              span: 'filled',
+              children: (
+                <Form.Item<RegisterUnitRequest> name="name" style={{ margin: 0 }}>
+                  <Input />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Unit Code',
+              children: (
+                <Form.Item<RegisterUnitRequest> name="code" style={{ margin: 0 }}>
+                  <Input />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Sub Department',
+              children: (
+                <Form.Item<RegisterUnitRequest> style={{ margin: 0 }} name="subDepartmentId">
+                  <Select style={{ width: '100%' }} options={subDepartmentOptions} />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Manager',
+              children: (
+                <Form.Item<RegisterUnitRequest> style={{ margin: 0 }}>
+                  <Select style={{ width: '100%' }} disabled />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Status',
+              children: (
+                <Form.Item<RegisterUnitRequest> name="status" style={{ margin: 0 }}>
+                  <Select
+                    style={{ width: '100%' }}
+                    options={[
+                      { label: 'Active', value: 'ACTIVE' },
+                      { label: 'Suspended', value: 'SUSPENDED' },
+                      { label: 'Closed', value: 'CLOSED' }
+                    ]}
+                  />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Description',
+              span: 'filled',
+              children: (
+                <Form.Item<RegisterUnitRequest> name="description" style={{ margin: 0 }}>
+                  <TextArea autoSize={{ minRows: 5 }} />
+                </Form.Item>
+              )
+            }
+          ]}
+        />
+      </Form>
+
+      <Flex justify="center" gap="middle">
+        <Button
+          icon={<LeftOutlined />}
+          variant="filled"
+          color="default"
+          onClick={() => navigate('/units')}
+        >
+          Cancel
+        </Button>
+
+        <Button
+          icon={<CheckOutlined />}
+          variant="filled"
+          color="primary"
+          onClick={submit}
+          loading={registerPending}
+        >
+          Register
+        </Button>
+      </Flex>
     </Flex>
   );
 };
