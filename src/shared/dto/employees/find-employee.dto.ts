c5af051a8283cb459dfe88b9578b323d26127df1
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-25 12:20:55 +0100

    Add find employee DTO

---
+++ src/shared/dto/employees/find-employee.dto.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+import { employeeStatuses } from '../../../main/db/schema';
+
+export const findEmployeeRequest = z.object({
+  page: z.coerce.number(),
+  name: z.string().nullable(),
+  code: z.string().nullable(),
+  organizationId: z.coerce.number().nullable(),
+  status: z.enum(employeeStatuses).nullable()
+});
+
+export const findEmployeeResponse = z.object({
+  total: z.number(),
+  items: z.array(
+    z.object({
+      id: z.number(),
+      firstName: z.string(),
+      lastName: z.string(),
+      code: z.string(),
+      email: z.string(),
+      status: z.enum(employeeStatuses)
+    })
+  )
+});
+
+export type FindEmployeeRequest = z.infer<typeof findEmployeeRequest>;
+export type FindEmployeeResponse = z.infer<typeof findEmployeeResponse>;
