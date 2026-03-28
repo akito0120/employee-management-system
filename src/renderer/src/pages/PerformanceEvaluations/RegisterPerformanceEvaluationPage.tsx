93df3391892d259983856ee6809f832fd5fd4d6f
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-28 19:54:10 +0100

    Implement register performance evaluation page

--- src/renderer/src/pages/PerformanceEvaluations/RegisterPerformanceEvaluationPage.tsx
+++ src/renderer/src/pages/PerformanceEvaluations/RegisterPerformanceEvaluationPage.tsx
@@ -1,21 +1,45 @@
 import { CheckOutlined, LeftOutlined } from '@ant-design/icons';
 import SelectEmployeeModal from '@renderer/components/SelectEmployeeModal';
+import { trpc } from '@renderer/trpc';
 import { Breadcrumb, Button, Descriptions, Flex, Form, Input } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FindEmployeeResponse } from 'src/shared/dto/employees/find-employee.dto';
+import { RegisterPerformanceEvaluationRequest } from 'src/shared/dto/performance-evaluations/register-performance-evaluation.dto';
+
+type FormType = Omit<
+  RegisterPerformanceEvaluationRequest,
+  'evaluatorEmployeeId' | 'evaluatedEmployeeId'
+>;
 
 const RegisterPerformanceEvaluationPage = () => {
   const navigate = useNavigate();
   const [evaluator, setEvaluator] = useState<FindEmployeeResponse['items'][number] | undefined>();
   const [evaluated, setEvaluated] = useState<FindEmployeeResponse['items'][number] | undefined>();
 
+  const { mutateAsync: register, isPending: registerPending } =
+    trpc.performanceEvaluations.registerPerformanceEvaluation.useMutation({
+      onSuccess: () => navigate(-1)
+    });
+  const [form] = Form.useForm<FormType>();
+
+  const submit = async () => {
+    const values = await form.validateFields();
+    if (!evaluator || !evaluated) return;
+
+    await register({
+      ...values,
+      evaluatorEmployeeId: evaluator.id,
+      evaluatedEmployeeId: evaluated.id
+    });
+  };
+
   return (
     <Flex vertical gap="large" style={{ padding: '2rem' }}>
       <Breadcrumb items={[{ title: 'Performance Evaluations' }, { title: 'Register' }]} />
 
-      <Form variant="filled">
+      <Form variant="filled" form={form}>
         <Descriptions
           bordered
           column={2}
@@ -35,7 +59,7 @@ const RegisterPerformanceEvaluationPage = () => {
             {
               label: '* Title',
               children: (
-                <Form.Item noStyle>
+                <Form.Item<FormType> noStyle name="title">
                   <Input />
                 </Form.Item>
               )
@@ -43,7 +67,7 @@ const RegisterPerformanceEvaluationPage = () => {
             {
               label: '* Score',
               children: (
-                <Form.Item noStyle>
+                <Form.Item<FormType> noStyle name="score">
                   <Input />
                 </Form.Item>
               )
@@ -51,7 +75,7 @@ const RegisterPerformanceEvaluationPage = () => {
             {
               label: '* Description',
               children: (
-                <Form.Item noStyle>
+                <Form.Item<FormType> noStyle name="description">
                   <TextArea autoSize={{ minRows: 20 }} />
                 </Form.Item>
               )
@@ -69,7 +93,14 @@ const RegisterPerformanceEvaluationPage = () => {
         >
           Cancel
         </Button>
-        <Button variant="filled" color="primary" icon={<CheckOutlined />}>
+
+        <Button
+          variant="filled"
+          color="primary"
+          icon={<CheckOutlined />}
+          onClick={submit}
+          loading={registerPending}
+        >
           Register
         </Button>
       </Flex>
