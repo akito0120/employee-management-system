e169dc79fdc807938e01dc7cab6943e7f1af8033
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-03-28 19:05:04 +0100

    Add register performance evaluation page basic layout

--- src/renderer/src/pages/PerformanceEvaluations/PerformanceEvaluationListPage.tsx
+++ src/renderer/src/pages/PerformanceEvaluations/PerformanceEvaluationListPage.tsx
@@ -1,10 +1,17 @@
-import { Breadcrumb, Flex } from 'antd';
+import { Breadcrumb, Button, Flex } from 'antd';
 import { JSX } from 'react/jsx-runtime';
+import { useNavigate } from 'react-router-dom';
 
 const PerformanceEvaluationListPage = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <Flex vertical gap="large" style={{ padding: '2rem' }}>
       <Breadcrumb items={[{ title: 'Performance Evaluations' }]} />
+
+      <Button variant="filled" color="primary" onClick={() => navigate('register')}>
+        Register
+      </Button>
     </Flex>
   );
 };
--- src/renderer/src/pages/PerformanceEvaluations/RegisterPerformanceEvaluationPage.tsx
+++ src/renderer/src/pages/PerformanceEvaluations/RegisterPerformanceEvaluationPage.tsx
@@ -1,10 +1,77 @@
-import { Breadcrumb, Flex } from 'antd';
-import { JSX } from 'react/jsx-runtime';
+import { CheckOutlined, LeftOutlined } from '@ant-design/icons';
+import { Breadcrumb, Button, Descriptions, Flex, Form, Input, Typography } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
+import { useNavigate } from 'react-router-dom';
 
-const RegisterPerformanceEvaluationPage = (): JSX.Element => {
+const RegisterPerformanceEvaluationPage = () => {
+  const navigate = useNavigate();
+
   return (
     <Flex vertical gap="large" style={{ padding: '2rem' }}>
       <Breadcrumb items={[{ title: 'Performance Evaluations' }, { title: 'Register' }]} />
+
+      <Form variant="filled">
+        <Descriptions
+          bordered
+          column={2}
+          items={[
+            {
+              label: 'Evaluator Employee',
+              children: (
+                <Button variant="filled" color="default" style={{ width: '100%' }}>
+                  <Typography.Text type="secondary">Select</Typography.Text>
+                </Button>
+              )
+            },
+            {
+              label: 'Evaluated Employee',
+              children: (
+                <Button variant="filled" color="default" style={{ width: '100%' }}>
+                  <Typography.Text type="secondary">Select</Typography.Text>
+                </Button>
+              )
+            },
+            {
+              label: 'Title',
+              children: (
+                <Form.Item noStyle>
+                  <Input />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Score',
+              children: (
+                <Form.Item noStyle>
+                  <Input />
+                </Form.Item>
+              )
+            },
+            {
+              label: 'Description',
+              children: (
+                <Form.Item noStyle>
+                  <TextArea autoSize={{ minRows: 20 }} />
+                </Form.Item>
+              )
+            }
+          ]}
+        />
+      </Form>
+
+      <Flex gap="middle" justify="center">
+        <Button
+          variant="filled"
+          color="default"
+          icon={<LeftOutlined />}
+          onClick={() => navigate(-1)}
+        >
+          Cancel
+        </Button>
+        <Button variant="filled" color="primary" icon={<CheckOutlined />}>
+          Register
+        </Button>
+      </Flex>
     </Flex>
   );
 };
