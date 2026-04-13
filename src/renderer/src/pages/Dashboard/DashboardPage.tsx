9b0f0ff5fe5fe39dd7e972fb0a54dcb36ec69e96
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-04-14 00:34:57 +0200

    Implement mock grid layout

--- src/renderer/src/pages/Dashboard/DashboardPage.tsx
+++ src/renderer/src/pages/Dashboard/DashboardPage.tsx
@@ -1,9 +1,46 @@
-import { Breadcrumb, Flex } from 'antd';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+import { Breadcrumb, Card, Flex, Statistic } from 'antd';
+import { format } from 'date-fns';
+import { useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
+import ReactGridLayout, { GridLayoutProps, useContainerWidth } from 'react-grid-layout';
+
+const dashboardLayoutAtom = atomWithStorage<GridLayoutProps['layout']>('dashboard-layout', [
+  { i: 'a', x: 0, y: 0, w: 3, h: 3, minW: 3, minH: 3 },
+  { i: 'b', x: 1, y: 0, w: 3, h: 3, minW: 3, minH: 3 },
+  { i: 'c', x: 4, y: 0, w: 3, h: 3, minW: 3, minH: 3 }
+]);
 
 const DashboardPage = () => {
+  const { width, containerRef, mounted } = useContainerWidth();
+  const [layout, setLayout] = useAtom(dashboardLayoutAtom);
+
   return (
     <Flex style={{ width: '100%', height: '100%', padding: '2rem' }} vertical gap="large">
       <Breadcrumb items={[{ title: 'Dashboard' }]} />
+
+      <div ref={containerRef}>
+        {mounted && (
+          <ReactGridLayout
+            layout={layout}
+            width={width}
+            gridConfig={{ cols: 12, rowHeight: 30, containerPadding: [0, 0] }}
+            onLayoutChange={(layout) => setLayout(layout)}
+          >
+            <Card key="a">
+              <Statistic title="Total Employee Count" value={13} suffix="employees" />
+            </Card>
+            <Card key="b">
+              <Statistic title="Date" value={format(new Date(), 'yyyy/MM/dd')} />
+            </Card>
+            <Card key="c">
+              <Statistic title="Average Salary" value={1000} prefix="€" />
+            </Card>
+          </ReactGridLayout>
+        )}
+      </div>
     </Flex>
   );
 };
